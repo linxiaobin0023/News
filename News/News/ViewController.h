352283f79a8e164947db9bbc7b32431456eeccaa
林晓斌 <linxiaobin@linxiaobindemacbook-pro.local>
2016-05-07 18:58:01 +0800

    第一次提交代码

---
+++ News/News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by 林晓斌 on 16/5/7.
+//  Copyright © 2016年 linxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ News/News/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  News
+//
+//  Created by 林晓斌 on 16/5/7.
+//  Copyright © 2016年 linxiaobin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
